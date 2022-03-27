08c7abdb43276108cb0e1a7920ea9fe9a8f8b9b7
Ajdin Hrelja <ajdin.hrelja@symphony.is>
2022-03-27 15:02:29 +0200

    optimizations

--- calculator.py
+++ calculator.py
@@ -1,4 +1,38 @@
-from numpy import sqrt
+from math import sqrt
+
+from numba import njit, prange
+
+import numpy as np
+
+
+@njit(nogil=True, fastmath=True)
+def calculate_distance(current_point, input_point):
+    res1 = (
+        (current_point[0] - input_point[0]) * (current_point[0] - input_point[0])
+        + (current_point[1] - input_point[1]) * (current_point[1] - input_point[1])
+        + (current_point[2] - input_point[2]) * (current_point[2] - input_point[2])
+    )
+    res2 = sqrt(res1)
+    return res2
+
+
+@njit(nogil=True, fastmath=True)
+def calculate_contribution(distance, propagation_factor, intensity_factor):
+    return intensity_factor / ((1 + distance) ** propagation_factor)
+
+
+@njit(nogil=True, fastmath=True)
+def calculate_value(current_point, holes, propagation_factor, intensity_factor):
+    total_contribution = 0
+
+    for holes in holes:
+        for h in range(holes[2] - holes[3] // 2, holes[2] + holes[3] // 2):
+            distance = calculate_distance(current_point=current_point, input_point=(holes[0], holes[1], h))
+            total_contribution += calculate_contribution(
+                distance=distance, propagation_factor=propagation_factor, intensity_factor=intensity_factor
+            )
+
+    return total_contribution
 
 
 class Calculator:
@@ -10,8 +44,10 @@ class Calculator:
         self.intensity_factor = intensity_factor
 
     def get_output(self, holes):
+        holes_tuples = np.array([(hole.x, hole.y, hole.z, hole.height) for hole in holes])
+
         output_matrix = self.__initialize_output_matrix()
-        output_matrix = self.__calculate_outputs(output_matrix, holes)
+        output_matrix = self.__calculate_outputs(output_matrix, holes_tuples, self.propagation_factor, self.intensity_factor)
 
         return (
             self.__flatten_by_x(output_matrix),
@@ -20,107 +56,67 @@ class Calculator:
         )
 
     def __initialize_output_matrix(self):
-        output_matrix = []
-
-        for _ in range(self.x_range):
-            x_array = []
-            for _ in range(self.y_range):
-                y_array = []
-                for _ in range(self.z_range):
-                    y_array.append(0)
-                x_array.append(y_array)
-            output_matrix.append(x_array)
+        output_matrix = np.zeros((self.x_range, self.y_range, self.z_range))
 
         return output_matrix
 
-    def __calculate_outputs(self, output_matrix, holes):
-        for x in range(self.x_range):
-            for y in range(self.y_range):
-                for z in range(self.z_range):
+    @staticmethod
+    @njit(parallel=True)
+    def __calculate_outputs(output_matrix, holes, propagation_factor, intensity_factor):
+        for x in prange(output_matrix.shape[0]):
+            for y in prange(output_matrix.shape[1]):
+                for z in prange(output_matrix.shape[2]):
                     current_point = (x, y, z)
-                    output_matrix[x][y][z] = self.__calculate_value(
+                    output_matrix[x][y][z] = calculate_value(
                         current_point=current_point,
                         holes=holes,
-                        propagation_factor=self.propagation_factor,
-                        intensity_factor=self.intensity_factor,
+                        propagation_factor=propagation_factor,
+                        intensity_factor=intensity_factor,
                     )
 
         return output_matrix
 
-    def __flatten_by_x(self, matrix):
-        flattened_output = self.__initialize_2d_matrix(x_range=self.y_range, y_range=self.z_range)
+    @staticmethod
+    @njit(parallel=True)
+    def __flatten_by_x(matrix):
+        flattened_output = np.zeros((150, 150))
 
-        for y in range(self.y_range):
-            for z in range(self.z_range):
-                flattened_value = 0
-                for x in range(self.x_range):
+        for y in prange(matrix.shape[1]):
+            for z in prange(matrix.shape[2]):
+                flattened_value = 0.0
+                for x in prange(matrix.shape[0]):
                     flattened_value += matrix[x][y][z]
 
                 flattened_output[y][z] = flattened_value
 
         return flattened_output
 
-    def __flatten_by_y(self, matrix):
-        flattened_output = self.__initialize_2d_matrix(x_range=self.x_range, y_range=self.z_range)
+    @staticmethod
+    @njit(parallel=True)
+    def __flatten_by_y(matrix):
+        flattened_output = np.zeros((matrix.shape[0], matrix.shape[2]))
 
-        for x in range(self.x_range):
-            for z in range(self.z_range):
-                flattened_value = 0
-                for y in range(self.y_range):
+        for x in prange(matrix.shape[0]):
+            for z in prange(matrix.shape[2]):
+                flattened_value = 0.0
+                for y in prange(matrix.shape[1]):
                     flattened_value += matrix[x][y][z]
 
                 flattened_output[x][z] = flattened_value
 
         return flattened_output
 
-    def __flatten_by_z(self, matrix):
-        flattened_output = self.__initialize_2d_matrix(x_range=self.x_range, y_range=self.y_range)
+    @staticmethod
+    @njit(parallel=True)
+    def __flatten_by_z(matrix):
+        flattened_output = np.zeros((matrix.shape[0], matrix.shape[1]))
 
-        for x in range(self.x_range):
-            for y in range(self.y_range):
-                flattened_value = 0
-                for z in range(self.z_range):
+        for x in prange(matrix.shape[0]):
+            for y in prange(matrix.shape[1]):
+                flattened_value = 0.0
+                for z in prange(matrix.shape[2]):
                     flattened_value += matrix[x][y][z]
 
                 flattened_output[x][y] = flattened_value
 
         return flattened_output
-
-    @staticmethod
-    def __initialize_2d_matrix(x_range, y_range):
-        flattened_output = []
-
-        for _ in range(x_range):
-            y_list = []
-            for _ in range(y_range):
-                y_list.append(0)
-            flattened_output.append(y_list)
-
-        return flattened_output
-
-    @classmethod
-    def __calculate_value(cls, current_point, holes, propagation_factor, intensity_factor):
-        total_contribution = 0
-
-        for holes in holes:
-            for h in range(holes.z - holes.height // 2, holes.z + holes.height // 2):
-                distance = cls.__calculate_distance(
-                    current_point=current_point, input_point=(holes.x, holes.y, h)
-                )
-                total_contribution += cls.__calculate_contribution(
-                    distance=distance, propagation_factor=propagation_factor, intensity_factor=intensity_factor
-                )
-
-        return total_contribution
-
-    @staticmethod
-    def __calculate_distance(current_point, input_point):
-        return sqrt(
-            (current_point[0] - input_point[0]) ** 2
-            + (current_point[1] - input_point[1]) ** 2
-            + (current_point[2] - input_point[2]) ** 2
-        )
-
-    @staticmethod
-    def __calculate_contribution(distance, propagation_factor, intensity_factor):
-        return intensity_factor / ((1 + distance) ** propagation_factor)
--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import time
-
+import numpy as np
 from calculator import Calculator
 from input_points_generator import HolesGenerator
 from plotting_helper import PlottingHelper
 
-X_RANGE = 50
-Y_RANGE = 50
-Z_RANGE = 50
-HEIGHT = 7
+X_RANGE = 30
+Y_RANGE = 30
+Z_RANGE = 30
+HEIGHT = 10
 PROPAGATION_FACTOR = 1.8
-INTENSITY_FACTOR = 10000
+INTENSITY_FACTOR = 10000.0
 
 
 if __name__ == "__main__":
-    holes = HolesGenerator.get_surface_row(
-        step=5, x_range=X_RANGE, y_range=Y_RANGE, z_range=Z_RANGE, height=HEIGHT
+    holes = np.array(
+        HolesGenerator.get_surface_row(step=5, x_range=X_RANGE, y_range=Y_RANGE, z_range=Z_RANGE, height=HEIGHT)
     )
     calculator = Calculator(
         x_range=X_RANGE,
@@ -23,12 +23,15 @@ if __name__ == "__main__":
         propagation_factor=PROPAGATION_FACTOR,
         intensity_factor=INTENSITY_FACTOR,
     )
+    print("Matrix size: " + str(X_RANGE * Y_RANGE * Z_RANGE))
+    print("Number of holes: " + str(len(holes)))
+    print("Height" + str(HEIGHT))
 
-    t = time.time()
+    start_time = time.time()
 
     output_x, output_y, output_z = calculator.get_output(holes=holes)
 
-    print(time.time() - t)
-    #
-    PlottingHelper.plot_input(input_points=holes, x_range=X_RANGE, y_range=Y_RANGE, z_range=Z_RANGE)
-    PlottingHelper.plot_ouputs(output_x, output_y, output_z)
+    print(f"Running time: {str(time.time() - start_time)}")
+
+    # PlottingHelper.plot_input(input_points=holes, x_range=X_RANGE, y_range=Y_RANGE, z_range=Z_RANGE)
+    # PlottingHelper.plot_ouputs(output_x, output_y, output_z)
